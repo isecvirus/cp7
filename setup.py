6baf3b0e12dfefc6f5a7e5100d4f5d1c4d456537
Virus <43806178+isecvirus@users.noreply.github.com>
2023-09-17 12:12:37 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="cpd",
-    version="1.0.0",
-    description="Cisco password type 7 decipher",
-    executables=[Executable("cpd.py")],
-)
